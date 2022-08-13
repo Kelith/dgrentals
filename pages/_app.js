58133e9ae186ce90b8149b7fa083b5ca3c405111
Kelith <garofalodavide7@gmail.com>
2022-08-13 11:22:12 +0200

    Added loader

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,16 @@ import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => NProgress.start())
+
+  Router.events.on('routeChangeComplete', () => NProgress.done())
+
   return (
     <>
       <Head>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' />
       </Head>
       <ChakraProvider>
         <Layout>
